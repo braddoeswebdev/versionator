49ea399fa29a40395d295655952dd8f5bbd1f4ce
Brad Thompson <brad@bradsotherbrain.com>
2012-05-06 09:19:37 -0400

    Set up rails app & gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Versionator::Application.config.secret_token = '4caed22008ba440beae37a526361f03947dce9fe0f56cbbb90c463fcc668ae7fc196c9728149b2c9427f57b3218470e5f4d02dcd8bc07e8614ec4048015bcdad'
